9b39a584de52914fc0938bf241f36e457dcdc014
Chintu <bcsf18m017@pucit.edu.pk>
2021-12-19 23:11:46 +0500

    Completed Database Commit

---
+++ app/src/main/java/haqnawaz/org/sqlitedb20211216/MainActivity.java
@@ -0,0 +1,76 @@
+package haqnawaz.org.sqlitedb20211216;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonAdd, buttonViewAll;
+    EditText editName, editAge;
+    Switch switchIsActive;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAdd = findViewById(R.id.buttonUpdate);
+        buttonViewAll = findViewById(R.id.buttonViewAll);
+        editName = findViewById(R.id.textName);
+        editAge = findViewById(R.id.textAge);
+        switchIsActive = findViewById(R.id.studentSwitch);
+        recyclerView = findViewById(R.id.recyclerViewStudents);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            StudentModel studentModel;
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    studentModel = new StudentModel(editName.getText().toString(), Integer.parseInt(editAge.getText().toString()), switchIsActive.isChecked());
+                    DbHelper dbHelper = new DbHelper(MainActivity.this);
+                    dbHelper.addStudent(studentModel);
+//                    Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "ADDED", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                ArrayList<StudentModel> list = dbHelper.getAllStudents();
+
+                recyclerView.setHasFixedSize(true);
+
+                //LinearLayoutManager GridLayoutManager
+                layoutManager = new LinearLayoutManager(MainActivity.this);
+                recyclerView.setLayoutManager(layoutManager);
+
+                adapter = new myRecyclerViewAdapter(list) ;
+                recyclerView.setAdapter(adapter);
+                //adapter.notifyDataSetChanged();
+
+
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/haqnawaz/org/sqlitedb20211216/StudentModel.java
@@ -0,0 +1,61 @@
+package haqnawaz.org.sqlitedb20211216;
+
+public class StudentModel {
+    private String name;
+    private int age;
+    private boolean isActive;
+    private int id;
+    public StudentModel(String name, int age, boolean isActive) {
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+     }
+    public StudentModel(int id,String name, int age, boolean isActive) {
+        this.id=id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return this.isActive;
+    }
+
+    public void setActive(boolean active) {
+        this.isActive = active;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                ", id=" + id +
+                '}';
+    }
+}
